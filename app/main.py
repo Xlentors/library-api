d54205d3a2981ed4db2c108c5de7bd397e225a90
Alexander Halim <igarashikaiyo@gmail.com>
2026-09-09 13:28:19 -0700

    adding book retrieval updates and deletion

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from datetime import date
 from pydantic import BaseModel
 
@@ -22,6 +22,12 @@ class BookResponse(BaseModel):
     available_copies: int
     is_active: bool
 
+class BookUpdate(BaseModel):
+    title: str | None = None
+    author: str | None = None
+    date_published: date | None = None
+    total_copies: int | None = None
+
 # Data
 
 books: list[dict] = []
@@ -35,6 +41,13 @@ def get_next_book_id() -> int:
         highest_id = max(highest_id, book["id"])
         
     return highest_id + 1    
+
+def find_book(book_id: int) -> dict | None:
+    for book in books:
+        if book["id"] == book_id:
+            return book
+        
+    return None
 
 # Main
 
@@ -58,5 +71,64 @@ def create_book(book: BookCreate):
 def get_books():
     return books
 
+@app.get("/books/{book_id}")
+def get_book(book_id: int):
+    book = find_book(book_id)
+    
+    if book is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Book not found"
+        )
+    
+    return book
 
+@app.patch("/books/{book_id}", response_model=BookResponse)
+def patch_book(book_id: int, book_update: BookUpdate):
+    book = find_book(book_id)
+    
+    if book is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Book not found"
+        )
+    
+    book_data = book_update.model_dump(
+        exclude_unset=True,
+        exclude_none=True
+    )
+    
+    if "total_copies" in book_data:
+        new_total = book_data["total_copies"]
+        
+        borrowed_copies = book["total_copies"] - book["available_copies"]
+        
+        if new_total < borrowed_copies:
+            raise HTTPException(
+                status_code=400,
+                detail="Total copies cannot be less than borrowed copies"
+            )
+            
+        book["available_copies"] = new_total - borrowed_copies
+                
+    for key, val in book_data.items():
+        book[key] = val
+
+    return book
+    
+@app.delete("/books/{book_id}", response_model=BookResponse)
+def delete_book(book_id: int):
+    book = find_book(book_id)
+    
+    if book is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Book not found"
+        )
+    
+    book["is_active"] = False
+    
+    return book
+        
+        
     
