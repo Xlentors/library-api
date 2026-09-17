79867e3b12f1be74403f14a9edd36d431548db89
Alexander Halim <igarashikaiyo@gmail.com>
2026-09-17 12:13:14 -0700

    adding tests to lending rules

--- tests/test_main.py
+++ tests/test_main.py
@@ -13,16 +13,16 @@ def test_health():
 def test_get_existing_book():
     response = client.get("/books/1")
     assert response.status_code == 200
-    
+
     response_body = response.json()
     assert response_body["id"] == 1
     assert response_body["title"] == "The Storm Before The Storm"
-    
+
 def test_get_missing_book():
     response = client.get("/books/991348")
     assert response.status_code == 404
     assert response.json() == {"detail": "Book not found"}
-    
+
 def test_get_book_with_invalid_id():
     response = client.get("/books/test")
     assert response.status_code == 422
